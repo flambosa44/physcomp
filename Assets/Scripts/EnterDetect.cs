399f61214f068f180629b85b18ff00717caf6003
flambosa44 <c.shield@uqconnect.edu.au>
2021-05-18 06:10:36 +1000

    Tentatively implemented detachment of marine animal model from Vuforia tracking target once placed in correct coral

--- Assets/Scripts/EnterDetect.cs
+++ Assets/Scripts/EnterDetect.cs
@@ -13,6 +13,8 @@ public class EnterDetect : MonoBehaviour
     private Animation animalAnimation;
     private AudioSource[] audioSources;
     public int vibrateValue;
+    private bool nearHome, detached;
+    private Transform homeTrans, animalTrans;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +22,10 @@ public class EnterDetect : MonoBehaviour
         animalAnimator = this.gameObject.GetComponent<Animator>();
         animalAnimation = this.gameObject.GetComponent<Animation>();
         bluetooth = GetComponentInChildren<ESP32>();
+        nearHome = false;
+        detached = false;
+        homeTrans = animalHome.transform;
+        animalTrans = this.gameObject.transform;
     }
 
     // Update is called once per frame
@@ -30,27 +36,87 @@ public class EnterDetect : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if (Global.AnimalNonColliders.Contains(other.gameObject.name))
+        if (detached || Global.AnimalNonColliders.Contains(other.gameObject.name))
             return;
         bool success = other.gameObject.name == animalHome.name;
         bluetooth.GetComponent<ESP32>().SetCollisionDetected(true, success ? 255 : 0);
         UpdateAnimation(success ? 0.5f : 2.0f);
         PlayAudio(other.gameObject.name == animalHome.name);
-        if(success)
-            canvasGood.SetActive(true);
+        PosTest.UpdateStatus("Enter Trigger");
+        if (success)
+        {
+            //canvasGood.SetActive(true);
+            nearHome = true;
+            StartCoroutine("SettleIn");
+        }
         else
-            canvasBad.SetActive(true);
+        {
+            //canvasBad.SetActive(true);
+        }
+    }
 
+    IEnumerator SettleIn()
+    {
+        Vector3 animalPos = animalTrans.position;
+        Vector3 animalRot = animalTrans.rotation.eulerAngles;
+        Vector3 homePos = homeTrans.position;
+        Vector3 homeRot = homeTrans.rotation.eulerAngles;
+        Vector3 deltaPos = animalPos - homePos;
+        Vector3 deltaRot = animalRot - homeRot;
+        Vector3 tempDeltaPos, tempDeltaRot;
+        float[] posTolerances = { 0.07f, 0.07f, 0.07f };
+        float[] rotTolerances = { 3f, 3f, 3f };
+        while (nearHome)
+        {
+            yield return new WaitForSecondsRealtime(3f);
+            if (!nearHome)
+                break;
+            animalPos = animalTrans.position;
+            homePos = homeTrans.position;
+            animalRot = animalTrans.rotation.eulerAngles;
+            homeRot = homeTrans.rotation.eulerAngles;
+            tempDeltaPos = animalPos - homePos;
+            tempDeltaRot = animalRot - homeRot;
+            PosTest.UpdateVectors(deltaPos, deltaRot);
+            if (AreSimilar(deltaPos, tempDeltaPos, posTolerances) && AreSimilar(tempDeltaRot, deltaRot, rotTolerances))
+                Detach();
+
+            else
+            {
+                deltaPos = animalPos - homePos;
+                deltaRot = animalRot - homeRot;
+            }
+            yield return null;
+        }
+    }
+
+    private bool AreSimilar(Vector3 vec1, Vector3 vec2, float[] tolerances)
+    {
+        return Mathf.Abs(vec1.x - vec2.x) <= tolerances[0]
+            && Mathf.Abs(vec1.y - vec2.y) <= tolerances[1]
+            && Mathf.Abs(vec1.z - vec2.z) <= tolerances[2];
+
+    }
+
+    private void Detach()
+    {
+        this.detached = true;
+        this.gameObject.transform.SetParent(null);
+        PosTest.UpdateStatus("Clownfish Detached!");
     }
 
     void OnTriggerExit(Collider other)
     {
-        if (Global.AnimalNonColliders.Contains(other.gameObject.name))
+
+        if (detached || Global.AnimalNonColliders.Contains(other.gameObject.name))
             return;
+        PosTest.UpdateStatus("Exit Trigger");
         bluetooth.GetComponent<ESP32>().SetCollisionDetected(false, vibrateValue);
-        canvasGood.SetActive(false);
-        canvasBad.SetActive(false);
+        //canvasGood.SetActive(false);
+        //canvasBad.SetActive(false);
         UpdateAnimation(1.0f);
+        if (nearHome)
+            nearHome = false;
     }
 
     void PlayAudio(bool success)
---
+++ Assets/Scripts/PosTest.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PosTest : MonoBehaviour
+{
+    public GameObject fish;
+    private Transform fishTrans, poolTrans;
+    private TextMeshProUGUI Xrot, Yrot, Zrot, Xvec, Yvec, Zvec, Mvec, status;
+    private static string statusText = "N/A";
+    private static Vector3 deltaPos = new Vector3(), deltaRot = new Vector3();
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextMeshProUGUI[] textMeshes = this.gameObject.GetComponentsInChildren<TextMeshProUGUI>();
+        foreach(TextMeshProUGUI textMesh in textMeshes)
+        {
+            if (textMesh.gameObject.name == "Xrot")
+                Xrot = textMesh;
+            else if (textMesh.gameObject.name == "Yrot")
+                Yrot = textMesh;
+            else if (textMesh.gameObject.name == "Zrot")
+                Zrot = textMesh;
+            else if (textMesh.gameObject.name == "Xvec")
+                Xvec = textMesh;
+            else if (textMesh.gameObject.name == "Yvec")
+                Yvec = textMesh;
+            else if (textMesh.gameObject.name == "Zvec")
+                Zvec = textMesh;
+            else if (textMesh.gameObject.name == "Mvec")
+                Mvec = textMesh;
+            else if (textMesh.gameObject.name == "Status")
+                status = textMesh;
+        }
+        //fishTrans = fish.transform;
+        //poolTrans = fish.GetComponent<EnterDetect>().animalHome.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 animalPos = fishTrans.position;
+        //Vector3 animalRot = fishTrans.rotation.eulerAngles;
+        //Vector3 homePos = poolTrans.position;
+        //Vector3 homeRot = poolTrans.rotation.eulerAngles;
+        //Vector3 deltaPos = animalPos - homePos;
+        //Vector3 deltaRot = animalRot - homeRot;
+
+        Xrot.text = "X: " + deltaRot.x;
+        Yrot.text = "Y: " + deltaRot.y;
+        Zrot.text = "Z: " + deltaRot.z;
+        Xvec.text = "X: " + deltaPos.x;
+        Yvec.text = "Y: " + deltaPos.y;
+        Zvec.text = "Z: " + deltaPos.z;
+        Mvec.text = "M: " + deltaPos.magnitude;
+        status.text = statusText;
+    }
+
+    public static void UpdateVectors(Vector3 extDeltaPos, Vector3 extDeltaRot)
+    {
+        deltaPos = extDeltaPos;
+        deltaRot = extDeltaRot;
+    }
+
+    public static void UpdateStatus(string message)
+    {
+        statusText = message;
+    }
+}
